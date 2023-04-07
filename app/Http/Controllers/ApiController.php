952314b59d51838af7ad37e7cc174a85843c620c
craftersama-meru <julmer.olivero@meru.com>
2023-04-07 18:48:55 -0400

    Improvements in user info

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Helper;
 use Illuminate\Http\Response;
-use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
@@ -18,18 +17,10 @@ class ApiController extends Controller
     {
         $user = User::with('professional_skills', 'technical_skills', 'services', 'featuredProjects', 'education', 'experiences', 'portfolios', 'posts', 'reviews', 'social_medias')->find($id);
 
-        if ($user->count() >= 1) {
-            return response()->json([
-                    'status' => 'ok',
-                    'user' => $user,
-                    'message' => Helper::successMessage('User info retrieved successfully')
-                ], Response::HTTP_OK);
-        } else {
-            return response()->json([
-                    'status' => 'error',
-                    'user' => null,
-                    'message' => Helper::errorMessage('User not found')
-                ], Response::HTTP_NOT_FOUND);
-        }
+        return response()->json([
+                'status' => $user ? 'ok' : 'error',
+                'user' => $user,
+                'message' => $user ? Helper::successMessage('User info retrieved successfully') : Helper::errorMessage('User not found')
+            ], $user ? Response::HTTP_OK : Response::HTTP_NOT_FOUND);
     }
 }
